7031efb2b0822541e12e5146b9badd34e0572242
Jax <ngjiaxun@gmail.com>
2023-07-29 22:26:03 +0800

    Create avatar and reload window

--- facebook-ads.js
+++ facebook-ads.js
@@ -1,4 +1,3 @@
-const SELECT_ONE = 'select one';
 const ADD_NEW = 'add new';
 const MAX_SUGGESTIONS = 20;
 const LOADING_MESSAGES = [
@@ -67,7 +66,6 @@ function runVue(avatars, solutions) {
                 avatars: avatars,
                 solution: solutions[0],
                 avatar: null,
-                avatarSelection: SELECT_ONE, 
                 avatarName: '',
                 painSuggestionIndex: 3,
                 desireSuggestionIndex: 3,
@@ -79,17 +77,12 @@ function runVue(avatars, solutions) {
         computed: {
             isAddNew() {
                 return this.avatarSelection === ADD_NEW;
-            },
-            isSelectOne() {
-                return this.avatarSelection === SELECT_ONE;
             }
         },
         methods: {
             avatarSelectionChanged() {
                 console.log(`Avatar ${this.avatarSelection} selected...`);
-                if (this.isSelectOne) {
-                    this.clearAvatar();
-                } else if (this.isAddNew) {
+                if (this.isAddNew) {
                     this.addNewAvatar();
                 } else {
                     this.retrieveAvatar(this.avatarSelection);
