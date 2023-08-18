8ed7d876c6692fd989c768b05c39d7b9b036cbb9
Jax <ngjiaxun@gmail.com>
2023-08-19 04:46:38 +0800

    Add copy-to-clipboard feature

--- facebook-ads.js
+++ facebook-ads.js
@@ -325,9 +325,8 @@ function runVue(avatars, solutions) {
             // },
             copyClicked(event) {
                 const button = event.currentTarget;
-                const copyid = button.dataset.copyId;
-                console.log('Copy clicked...', copyid);
-                const text = document.getElementById(copyid).innerText;
+                const copyId = button.dataset.copyid;
+                const text = document.getElementById(copyId).innerText;
                 navigator.clipboard.writeText(text)
                     .then(() => console.log('Text copied to clipboard:', text))
                     .catch(error => console.error('Error copying text to clipboard:', error.message));
