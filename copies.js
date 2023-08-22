6f04124154437d709158c9fe449fa0ef2f72b23d
Jax <ngjiaxun@gmail.com>
2023-08-23 06:35:33 +0800

    Move code to copies.js

---
+++ copies.js
@@ -0,0 +1,82 @@
+const COUNTDOWN_MESSAGE = [
+    'Ready in ',
+    '5',
+    '.',
+    '.',
+    '.',
+    ' ',
+    '4',
+    '.',
+    '.',
+    '.',
+    ' ',
+    '3',
+    '.',
+    '.',
+    '.',
+    ' ',
+    '2',
+    '.',
+    '.',
+    '.',
+    ' ',
+    '1',
+    '.',
+    '.',
+    '.',
+    ' ',
+    'Any',
+    ' ',
+    'second',
+    ' ',
+    'now',
+    '!',
+    '.',
+    '.',
+    '.',
+    ' '
+];
+
+const copies = {
+    data: {
+        countdownMessage: '',
+    },
+    methods: {
+        async startCountdown(copy) {
+            console.log('Restarting countdown...');
+            this.countdownMessage = '';
+            for (let i = 0; i < COUNTDOWN_MESSAGE.length; i++) {
+                this.countdownMessage += COUNTDOWN_MESSAGE[i];
+                await delay(1000);
+                if (!copy.isLoading) {
+                    break;
+                }
+            }
+        },
+        async generateCopy(copy, generationEndpoint, checkingEndpoint, payload) {
+            console.log('Generating copy...', copy);
+            const requestedTime = new Date().toISOString(); // Timestamp for identifying the copy after it's generated
+            payload.requested_time = requestedTime;
+            await axios.post(generationEndpoint, payload);
+            copy.isLoading = true; 
+            this.startCountdown(copy);
+            copy.copy = await this.checkCopyReady(requestedTime, checkingEndpoint);
+            copy.isLoading = false; 
+        },
+        async checkCopyReady(requestedTime, endpoint, maxTries=DEFAULT_MAX_TRIES, timeout=DEFAULT_TIMEOUT) {
+            console.log('Checking if copy is ready...', requestedTime)
+            endpoint += '?requested_time=' + requestedTime;
+            let tries = 0; 
+            while (tries < maxTries) {
+                const response = await axios.get(endpoint);
+                if (response.data.length > 0) {
+                    return response.data[0];
+                } else {
+                    await delay(timeout); 
+                    tries++;
+                    console.log('Tries:', tries, '/', maxTries);
+                }
+            }
+        }
+    }
+};
--- facebook-ads.js
+++ facebook-ads.js
@@ -54,7 +54,7 @@ function runVue(avatars, solutions) {
 
                 tries: 0, // Current number of tries to load the avatar
 
-                countdownMessage: '', 
+                ...copies.data,
 
                 copies: {
                     avatar: {
@@ -104,6 +104,9 @@ function runVue(avatars, solutions) {
             }
         },
         methods: {
+            startCountdown: copies.methods.startCountdown,
+            generateCopy: copies.methods.generateCopy,
+            checkCopyReady: copies.methods.checkCopyReady,
             avatarSelectionChanged() {
                 console.log(`Avatar ${this.avatarSelection} selected...`);
                 if (this.isSelectOne) {
@@ -233,43 +236,6 @@ function runVue(avatars, solutions) {
                         copy.copy.copy = undefined;
                     }
                 });
-            },
-            async startCountdown(copy) {
-                console.log('Restarting countdown...');
-                this.countdownMessage = '';
-                for (let i = 0; i < COUNTDOWN_MESSAGE.length; i++) {
-                    this.countdownMessage += COUNTDOWN_MESSAGE[i];
-                    await delay(1000);
-                    if (!copy.isLoading) {
-                        break;
-                    }
-                }
-            },
-            
-            async generateCopy(copy, generationEndpoint, checkingEndpoint, payload) {
-                console.log('Generating copy...', copy);
-                const requestedTime = new Date().toISOString(); // Timestamp for identifying the copy after it's generated
-                payload.requested_time = requestedTime;
-                await axios.post(generationEndpoint, payload);
-                copy.isLoading = true; 
-                this.startCountdown(copy);
-                copy.copy = await this.checkCopyReady(requestedTime, checkingEndpoint);
-                copy.isLoading = false; 
-            },
-            async checkCopyReady(requestedTime, endpoint, maxTries=DEFAULT_MAX_TRIES, timeout=DEFAULT_TIMEOUT) {
-                console.log('Checking if copy is ready...', requestedTime)
-                endpoint += '?requested_time=' + requestedTime;
-                let tries = 0; 
-                while (tries < maxTries) {
-                    const response = await axios.get(endpoint);
-                    if (response.data.length > 0) {
-                        return response.data[0];
-                    } else {
-                        await delay(timeout); 
-                        tries++;
-                        console.log('Tries:', tries, '/', maxTries);
-                    }
-                }
             },
             copyClicked(event) {
                 copyToClipboard(event);
