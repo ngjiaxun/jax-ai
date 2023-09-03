4cd8ff681dd901d5567815e9ddccdf560530584b
Jax <ngjiaxun@gmail.com>
2023-09-03 11:10:06 +0800

    Refactor avatars.js to use scaffolding.js

--- avatars.js
+++ avatars.js
@@ -34,23 +34,3 @@ function runVue(user, avatars) {
         }
     }).mount('#app')
 }
-
-
-// function runVue(data) {
-//   const { createApp } = Vue
-//   createApp({
-//     data() {
-//       return {
-//         avatars: data
-//       }
-//     },
-//     methods: {
-//     },
-//     mounted() {
-//     }
-//   }).mount('#app')
-// }
-
-// axios.get(apiEndpoints.avatars)
-//   .then(response => runVue(response.data))
-//   .catch(error => console.error('Error fetching data:', error.message));
