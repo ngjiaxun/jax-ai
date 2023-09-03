0fd059f795ec9bb91acd8c2a07e3799c625b507a
Jax <ngjiaxun@gmail.com>
2023-09-03 10:49:58 +0800

    Refactor avatars.js to use scaffolding.js

--- avatars.js
+++ avatars.js
@@ -1,7 +1,7 @@
 (async function main() {
     try {
         const user = await preInit();
-        const avatars = await axios.get(apiEndpoints.avatars);
+        const avatars = await axios.get(endpoints.avatars);
         runVue(user, avatars.data);
     } catch (error) {
         console.error('Error initializing Jax AI:', error.message);
