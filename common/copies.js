7d22ca784a373741ad8e6672b9ecffb86b07062c
Jax <ngjiaxun@gmail.com>
2023-10-06 04:43:58 +0800

    Add label fields to generateCopies() in facebook-ads.js

--- common/copies.js
+++ common/copies.js
@@ -173,7 +173,7 @@ const copies = {
             }
         },
         getCopyTitle(copy) {
-            const label = copy.data.label ? ' - ' + copy.data.label : '';
+            const label = copy.data.label ? ' ' + copy.data.label : '';
             const copyType = LEFT_BRACKET + copy.data.original_copy_type_display + label + RIGHT_BRACKET;
             return `${copyType} ${copy.data.transformation ?? ''} ${copy.data.transformation_copy_type_display ?? ''}`;
         },
