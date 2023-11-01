d86c38cbc284310d34580bf384a6e9a38c9891b9
Jax <ngjiaxun@gmail.com>
2023-11-01 19:16:22 +0800

    Add Copy.error to copies.js

--- common/settings.js
+++ common/settings.js
@@ -1,6 +1,6 @@
 const IDLE_EXPIRY_MINUTES = 240; // The number of minutes before the user is required to login again
 const DEFAULT_TIMEOUT = 5000; // The time to wait between checks if a copy is ready
-const DEFAULT_MAX_TRIES = 24; // The maximum number of times to check if a copy is ready
+const DEFAULT_MAX_TRIES = 12; // The maximum number of times to check if a copy is ready
 
 const RECAPTCHA_SITE_KEY = '6Ldwq_knAAAAAG3LpMIRYzyHPvJxzXeOJgfJGfKi';
 
