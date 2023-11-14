a941c3c157c00860a34eddc4dcbffb317b30f330
Jax <ngjiaxun@gmail.com>
2023-11-15 01:37:59 +0800

    Add forgot-password-confirm.js

--- common/authentication.js
+++ common/authentication.js
@@ -38,7 +38,7 @@ async function resetPassword(email) {
 async function resetPasswordConfirm(uid, token, password) {
     let success = false;
     try {
-        const response = await axios.post(endpoints.resetPasswordConfirm, { uid: uid, token: token, password: password });
+        const response = await axios.post(endpoints.resetPasswordConfirm, { uid: uid, token: token, new_password: password });
         console.log('Reset password confirm response:', response.data);
         success = true;
     } catch (error) {
